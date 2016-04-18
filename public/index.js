22a58cce14dc7d7fd68e1bd197bb8770bdd53577
betodt <alberto.diaz.tostado@gmail.com>
2016-04-18 12:40:40 -0700

    changed request url

--- public/index.js
+++ public/index.js
@@ -124,7 +124,7 @@ var mixedPercentage;
                   }
 
                   $http({
-                    url: "http://localhost:3000",
+                    url: $location.path(),
                     method: "POST",
                     data: data,
                     dataType: 'jsonp'
